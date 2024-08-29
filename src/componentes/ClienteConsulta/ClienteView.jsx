020b0192eea3451a4be537def8e6cac29c371102
almeida1 <engsoftware1@gmail.com>
2024-08-29 08:45:31 -0300

    funcao - cosulta todos os clientes  On branch master  Changes to be committed: 	modified:   src/App.js 	new file:   src/componentes/ClienteConsulta/ClienteView.jsx 	new file:   src/componentes/ClienteConsulta/index.jsx 	new file:   src/componentes/ClienteConsulta/styles.css

---
+++ src/componentes/ClienteConsulta/ClienteView.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.css"; // Importa o arquivo de estilos
+
+const ClienteView = ({ clientes }) => {
+  return (
+    <div className="cliente-view">
+      <h3>Lista de Clientes</h3>
+      {clientes.length > 0 ? (
+        <table className="cliente-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Endereço</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <tr key={cliente.id}>
+                <td>{cliente.id}</td>
+                <td>{cliente.nome}</td>
+                <td>{cliente.endereco}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Nenhum cliente encontrado</p>
+      )}
+    </div>
+  );
+};
+
+export default ClienteView;
---
+++ src/componentes/ClienteConsulta/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import ClienteView from "./ClienteView";
+
+const ClienteConsulta = () => {
+  const [clientes, setClientes] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCliente = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/v1/clientes/all"
+        );
+        if (!response.ok) {
+          throw new Error("Erro na requisição");
+        }
+        const data = await response.json();
+        setClientes(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCliente();
+  }, []);
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (error) {
+    return <div>Erro: {error}</div>;
+  }
+
+  return clientes ? (
+    <ClienteView clientes={clientes} />
+  ) : (
+    <div>Nenhum cliente encontrado</div>
+  );
+};
+
+export default ClienteConsulta;
