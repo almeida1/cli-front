55107134aa78abb4d2ecce24b57eff465b543a4c
Edson Saraiva de Almleida <engsoftware1@gmail.com>
2024-09-18 12:05:59 -0300

    Funcao consulta - refatoracao endpoint do back

--- src/componentes/consulta/ClienteConsulta.jsx
+++ src/componentes/consulta/ClienteConsulta.jsx
@@ -10,7 +10,7 @@ const ClienteConsulta = () => {
     const fetchCliente = async () => {
       try {
         const response = await fetch(
-          "https://cli-back-998bc5582c3e.herokuapp.com/"
+          "https://cli-back-998bc5582c3e.herokuapp.com/all"
         );
         if (!response.ok) {
           throw new Error("Erro na requisição");
