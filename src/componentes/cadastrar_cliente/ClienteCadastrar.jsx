1e40d208a1423e9cb8c0cf20cc7d08b310ef34be
almeida1 <engsoftware1@gmail.com>
2024-10-11 09:42:00 -0300

    funcao cadastrar_cliente - falha cpf ja cadastrado  Changes to be committed: 	modified:   src/componentes/cadastrar_cliente/ClienteCadastrar.jsx

--- src/componentes/cadastrar_cliente/ClienteCadastrar.jsx
+++ src/componentes/cadastrar_cliente/ClienteCadastrar.jsx
@@ -1,12 +1,16 @@
 const ClienteCadastrar = async (cliente) => {
   try {
-    const response = await fetch("http://localhost:8080/api/v1/clientes", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(cliente),
-    });
+    const response = await fetch(
+      //"http://localhost:8080/api/v1/clientes",
+      "https://cli-back-da94521f4063.herokuapp.com/api/v1/clientes",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cliente),
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Falha ao cadastrar cliente");
